00708e0184e639795a68bd079ad707df79ed5a29
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-09-05 04:31:42 +0000

    Add minimal MQTT server scaffold

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <fmt/core.h>
 
+#include <warp/mqtt/server.h>
+
 int main() {
   fmt::print("OK\n");
+  warp::mqtt::Server server;
+  server.start();
   return 0;
 }
---
+++ src/warp/mqtt/server.cpp
@@ -0,0 +1,12 @@
+#include <fmt/core.h>
+
+#include <warp/mqtt/server.h>
+
+namespace warp::mqtt {
+Server::Server() {}
+Server::~Server() {}
+
+void Server::start() {
+    fmt::print("Server::start\n");
+}
+}  // namespace warp
