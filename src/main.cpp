20b8d414bed75d08be567e517f8b17149736aa3a
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-09-13 15:58:10 -0700

    Add server options

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <fmt/core.h>
 #include <warp/mqtt/server.h>
 
+#include <thread>
+
 int main() {
   fmt::print("OK\n");
-  warp::mqtt::Server server;
-  server.start();
+  warp::mqtt::ServerOptions options;
+  warp::mqtt::Server server(options);
+  std::thread t([&]() { server.start(); });
+  t.join();
   return 0;
 }
