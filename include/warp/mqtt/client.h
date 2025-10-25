42a8302bf7524d6a64b396d3e6fa781570741fca
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-10-25 01:16:42 +0000

    Add client service

--- include/warp/mqtt/client.h
+++ include/warp/mqtt/client.h
@@ -1,19 +1,28 @@
 #pragma once
 
+#include <folly/futures/Future.h>
+
 #include <memory>
 #include <string>
+
+#include "warp/mqtt/codec.h"
 
 namespace warp::mqtt {
 class ClientOptions {
 public:
-    std::string host{"127.0.0.1"};
-    uint16_t port{1883};
+  std::string host{"127.0.0.1"};
+  uint16_t port{1883};
 };
 
 class Client final {
 public:
-    explicit Client(ClientOptions const& options);
-    virtual ~Client();
+  explicit Client(ClientOptions const& options);
+  virtual ~Client();
+
+  void connect();
+  void close();
+
+  folly::Future<Message> request(Message msg);
 
 private:
   std::shared_ptr<ClientOptions> options_;
