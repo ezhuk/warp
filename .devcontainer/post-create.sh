4d96c115934e422502e5d5067ea371ea2a0c09dc
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-11-12 01:27:16 +0000

    Add mqtt test tool

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,6 +3,10 @@ set -euo pipefail
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+curl -LO https://www.emqx.com/en/downloads/MQTTX/v1.12.1/mqttx-cli-linux-x64
+sudo install mqttx-cli-linux-x64 /usr/local/bin/mqttx
+rm mqttx-cli-linux-x64
+
 if [ -n "${VCPKG_ROOT:-}" ]; then
   sudo rm -rf "${VCPKG_ROOT}"
   sudo git clone https://github.com/microsoft/vcpkg "${VCPKG_ROOT}"
