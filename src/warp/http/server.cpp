2ed7e26b901292ea387eec78b736c8620b8ec493
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2025-11-15 22:12:57 +0000

    Update http server

--- src/warp/http/server.cpp
+++ src/warp/http/server.cpp
@@ -1,17 +1,17 @@
 #include "warp/http/server.h"
 
 #include <folly/system/HardwareConcurrency.h>
-#include <proxygen/httpserver/HTTPServer.h>
 #include <proxygen/httpserver/filters/DirectResponseHandler.h>
-
-#include <unordered_map>
 
 namespace warp::http {
 namespace {
-std::unordered_map<std::string, std::shared_ptr<proxygen::RequestHandlerFactory>> handlers;
-
 class HandlerFactory final : public proxygen::RequestHandlerFactory {
 public:
+  explicit HandlerFactory(
+      std::unordered_map<std::string, std::shared_ptr<proxygen::RequestHandlerFactory>>* handlers
+  )
+      : handlers_(handlers) {}
+
   void onServerStart(folly::EventBase*) noexcept override {}
 
   void onServerStop() noexcept override {}
@@ -20,16 +20,17 @@ public:
       proxygen::RequestHandler* h, proxygen::HTTPMessage* msg
   ) noexcept override {
     auto const& path = msg->getPath();
-    for (auto const& [p, handler] : handlers) {
+    for (auto const& [p, handler] : *handlers_) {
       if (folly::StringPiece(path).startsWith(p)) {
         return handler->onRequest(h, msg);
       }
     }
     return new proxygen::DirectResponseHandler(404, "Not Found", "{\"error\":\"Not Found\"}");
   }
-};
 
-std::shared_ptr<proxygen::HTTPServer> server;
+private:
+  std::unordered_map<std::string, std::shared_ptr<proxygen::RequestHandlerFactory>>* handlers_;
+};
 }  // namespace
 
 Server::Server(ServerOptions const& options) : options_(std::make_shared<ServerOptions>(options)) {
@@ -38,27 +39,34 @@ Server::Server(ServerOptions const& options) : options_(std::make_shared<ServerO
   }
 }
 
-Server::~Server() {}
+Server::~Server() = default;
 
 void Server::start() {
-  proxygen::HTTPServerOptions options;
-  options.threads = options_->threads;
-  options.idleTimeout = std::chrono::seconds(60);
-  options.supportsConnect = true;
-  options.handlerFactories = proxygen::RequestHandlerChain().addThen<HandlerFactory>().build();
-  server = std::make_shared<proxygen::HTTPServer>(std::move(options));
-  server->bind(
-      {{folly::SocketAddress("0.0.0.0", options_->port, true),
-        proxygen::HTTPServer::Protocol::HTTP}}
-  );
-  server->start();
+  if (!server_) {
+    proxygen::HTTPServerOptions options;
+    options.threads = options_->threads;
+    options.idleTimeout = options_->timeout;
+    options.handlerFactories =
+        proxygen::RequestHandlerChain().addThen<HandlerFactory>(&handlers_).build();
+    server_ = std::make_shared<proxygen::HTTPServer>(std::move(options));
+    server_->bind(
+        {{folly::SocketAddress("0.0.0.0", options_->port, true),
+          proxygen::HTTPServer::Protocol::HTTP}}
+    );
+    server_->start();
+  }
 }
 
-void Server::stop() { server->stop(); }
+void Server::stop() {
+  if (server_) {
+    server_->stop();
+    server_.reset();
+  }
+}
 
 void Server::addHandler(
     std::string const& path, std::shared_ptr<proxygen::RequestHandlerFactory> handler
 ) {
-  handlers.emplace(std::move(path), std::move(handler));
+  handlers_.emplace(std::move(path), std::move(handler));
 }
 }  // namespace warp::http
